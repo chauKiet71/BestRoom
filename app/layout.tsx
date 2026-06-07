e14f0ca3892b681542274e50983f3dd229046ed9
= <=>
2026-06-07 09:31:59 +0700

    add code tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { AppContextProvider } from "@/context/AppContext";
 import LayoutWrapper from "@/components/LayoutWrapper";
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "BestRoom - Hệ Thống Tìm Kiếm Phòng Trọ Giá Tốt",
@@ -20,6 +21,15 @@ export default function RootLayout({
   return (
     <html lang="vi">
       <body className="font-sans antialiased min-h-screen bg-gray-50/50 text-gray-800 flex flex-col">
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "x2wfyt2u5x");
+          `}
+        </Script>
         <AppContextProvider>
           <LayoutWrapper>
             {children}
@@ -29,3 +39,4 @@ export default function RootLayout({
     </html>
   );
 }
+
